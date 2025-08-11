73bed22c91cdc7b2d48e8b70782830bbca681e99
Nikola Laboski <nikolchelaboski.dev@gmail.com>
2025-08-11 19:50:13 +0200

    Force new build: proxy /graphql + https guard

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,6 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-export default new ApolloClient({
-  uri: '/graphql', 
-  cache: new InMemoryCache(),
-});
+let uri = '/graphql'; // default преку Netlify proxy
+
+if (typeof window !== 'undefined' &&
+    window.location.protocol === 'https:' &&
+    uri.startsWith('http:')) {
+  uri = '/graphql';
+}
+
+console.log('Apollo URI =', uri); 
+export default new ApolloClient({ uri, cache: new InMemoryCache() });
