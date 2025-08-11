7b46ac10f706f27366869f07b65c4c9ff26108de
Nikola Laboski <nikolchelaboski.dev@gmail.com>
2025-08-11 15:55:39 +0200

    d

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,4 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const uri = import.meta.env.VITE_API_URL ?? '/graphql'; // default: proxy преку Netlify
-export default new ApolloClient({ uri, cache: new InMemoryCache() });
+export default new ApolloClient({
+  uri: '/graphql', 
+  cache: new InMemoryCache(),
+});
