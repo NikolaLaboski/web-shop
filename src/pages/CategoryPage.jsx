747a52e71f9904a7249d8b8ee7bdada52ddab421
NikolaLaboski <nikolchelaboski.dev@gmail.com>
2025-09-17 00:47:34 +0200

    sd

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -129,7 +129,7 @@ const OutOfStockBadge = styled.span`
 `;
 const AddToCartButton = styled.button`
   position: absolute;
-  bottom: -56px;
+  bottom: -12px;
   right: 12px;
   width: 52px;
   height: 52px;
